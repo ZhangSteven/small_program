96f8302ff4bf032b408a68af4948af138971a0e8
Steven Zhang <steven.shutao@gmail.com>
2019-05-13 19:21:44 +0800

    added tee() and accumulator()

--- generator.py
+++ generator.py
@@ -5,7 +5,8 @@
 # About generator function
 
 from itertools import takewhile
-from itertools import islice
+from itertools import islice, tee, accumulate
+from operator import mul
 
 
 def get_primes():
@@ -20,6 +21,14 @@ def get_primes():
             found.append(candidate)
 
         candidate += 1
+
+
+
+def fabonacci():
+    a, b = 0, 1
+    while True:
+        yield a 
+        a, b = b, a+b 
 
 
 
@@ -48,6 +57,26 @@ if __name__ == '__main__':
 
 
     """
-    Using the built in library is the easiest to get first 10 elements
+    Using the built in 'islice' function, to retrieve the elements of index
+    [0, 10)    
     """
     print(list(islice(get_primes(), 0, 10)))
+
+
+
+    """
+    The new problem: find out the prime number, which makes the product of
+    all prime numbers equal or smaller than it > 1,000
+
+    prime   sum of sqaures
+    2       2
+    3       6
+    5       30
+    ...
+    ?       > 1,000
+
+    """
+    s, t = tee(get_primes())
+    pairs = zip(t, accumulate(s, mul))
+    for (prime, total) in takewhile(lambda x: x[1] < 1000, pairs):
+        print(prime, total)
